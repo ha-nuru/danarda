a4eae18e60337cb8ed73cb133310c4a9b06f9d8e
ha-nuru <gkdk4195@gmail.com>
2021-03-31 18:11:29 +0900

    init

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ $(function () {
         $(this).parents('li').addClass('active').siblings().removeClass('active');
         $('.bubble_text').eq(idx).addClass('active').siblings().removeClass('active');
         $('.thr_mob').hide().eq(idx).show();
-
     })
 
     $('.bubble_text').on('click', function () {
@@ -16,30 +15,10 @@ $(function () {
             $(this).addClass('active').siblings().removeClass('active');
             $('.line_number_wrap li').eq(idx).addClass('active').siblings().removeClass('active');
             $('.thr_mob').hide().eq(idx).show();
-            console.log(idx);
         }
     })
     // 스크롤 애니
-    $(window).scroll(function () {
-        let scroll = $(window).scrollTop() + 400;
-        if (scroll >= $(".chat_content").offset().top) {
-            var current_height = $(".phone_wrap_ani").prop("scrollHeight");
-            $(".phone_wrap_ani li:nth-child(1)").show("drop", { direction: "down", easing: 'easeInOutExpo' }, 1000, function () {
-                $(".phone_wrap_ani li:nth-child(2)").delay(600).show("drop", { direction: "down", easing: 'easeInOutCirc' }, 1000, function () {
-                    $(".phone_wrap_ani li:nth-child(3)").delay(600).show("drop", { direction: "down", easing: 'easeInOutCirc' }, 1000, function () {
-                        $(".phone_wrap_ani").animate({ top: '-15%' }, 600, function () {
-                            $(".phone_wrap_ani li:nth-child(4)").delay(600).show("drop", { direction: "down", easing: 'easeInOutCirc' }, 1000, function () {
-                                $(".phone_wrap_ani li:nth-child(5)").delay(600).show("drop", { direction: "down", easing: 'easeInOutCirc' }, 1000, function () {
-                                    $(".phone_wrap_ani li:nth-child(6)").delay(600).show("drop", { direction: "down", easing: 'easeInOutCirc' }, 1000, function () {
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        }
-    });
+	
     // top 버튼
     $('.mov_top').on('click', function () {
         $('html, body').animate({
